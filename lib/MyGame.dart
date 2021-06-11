228d110d2696f0a69d53c7a9f646ea5f4fa14d9c
Kweku <kwekuappiah11@gmail.com>
2021-06-11 23:10:25 +0000

    initial commit

---
+++ lib/MyGame.dart
@@ -0,0 +1,18 @@
+
+
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class MyGame extends Game{
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:first_game/MyGame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  final game = MyGame();
+  runApp(GameWidget(
+    game: game
+  ));
+}
